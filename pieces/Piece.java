5f5d83f1eaabd46cba280eb76eaa4bab26dc61ec
Lennon Kucharski <lennonporto@gmail.com>
2024-12-04 23:43:40 -0300

    new move method on piece

--- pieces/Piece.java
+++ pieces/Piece.java
@@ -6,6 +6,7 @@ import game.Move;
 
 public abstract class Piece {
     protected int x, y;
+
     protected Color color;
     protected char identifier;
 
@@ -23,6 +24,23 @@ public abstract class Piece {
         return this.identifier;
     };
 
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
     public abstract boolean isValidMove(Move move, Board board);
+
+    public void move(Move move, Board board) throws Exception {
+        if (!this.isValidMove(move, board)) {
+            throw new Exception("Not a valid move");
+        }
+
+        this.setX(move.getLinhaFinal());
+        this.setY(move.getColunaFinal());
+    }
 
 }
